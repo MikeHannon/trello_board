d2e5e62e933b787ae281e3171f0f3391cc1a0afd
Mike Hannon <mjhannon@gmail.com>
2017-03-16 10:28:11 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component,PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import DATA from '../card_data.json';
 
 class App extends Component {
+  static PropTypes= {
+    cards: PropTypes.array.isRequired,
+  }
+ constructor(props){
+   super(props);
+   const statuses = new Set()
+   props.cards.forEach((cardObj)=>{
+     statuses.add(cardObj.status)
+   })
+   this.state = {
+     statuses
+   }
+ }
+
   render() {
-    const cardTitles = DATA.cards.map((card) => <p key={card.id}>{card.title}</p>);
+    const cardContainers = Array.from(this.state.statuses).map(
+      status => <CardContainer title = {status} key={status} />)
     return (
       <div>
-        {cardTitles}
+      {cardContainers}
       </div>
     )
   }
 }
 
+const CardContainer = (props)=>{
+  return <h1>{props.status}</h1>
+}
+const Card  = (props)=>{
+  return <h1>Card</h1>
+}
+const CardHeader  = (props)=>{
+  return <h1>CardHeader</h1>
+}
+const CardChecklist  = (props)=>{
+  return <h1>CardChecklist</h1>
+}
 ReactDOM.render(
-  <App />,
+  <App cards={DATA.cards}/>,
   document.getElementById('root')
 );
