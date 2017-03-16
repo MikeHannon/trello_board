c34813b61ba6d76c1fa04f999cbee53247036d11
Charles Mead <meadch@gmail.com>
2017-03-16 11:52:24 -0700

    updates styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,21 @@
 import React, { PropTypes } from 'react';
 import './card.css';
 
-const Card = ({status}) => {
+const Card = ({status, title, description}) => {
   return (
     <div className={`card ${status}`}>
-      {status}      
+      <header>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </header> 
     </div>
   )
 }
 
 Card.propTypes = {
-  status: PropTypes.string.isRequired
+  status: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
 }
 
 export default Card;
