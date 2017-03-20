62577293f4afc69ac0a91b6d3a61371f5cf9b0e1
Mike Hannon <mjhannon@gmail.com>
2017-03-20 09:43:36 -0700

    backed-up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,12 @@
 import React, { PropTypes } from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
 import './card.css';
+
+const cardSource = {
+  beginDrag(props){
+
+  }
+}
 
 const Card = ({status, title, description}) => {
   return (
